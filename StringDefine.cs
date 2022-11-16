bca5674278cb202b12775bfbf92f36009d9c00b8
Hoang Minh Chien <Kydan301193@gmail.com>
2022-11-16 15:15:46 +0700

    first commit

---
+++ StringDefine.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkymareGamePuzzle
+{
+    public static class StringDefine
+    {
+        public static string LARGEST_NUMBER = "largest_number";
+        public static string SMALLEST_NUMBER = "smallest_number";
+        public static string CURRENT_GAME_SCORE = "current_game_score";
+        public static string HIGHEST_SCORE = "highest_score";
+        //public static string SCORE_PER_CONNECT = "score_per_connect";
+
+        public static string LARGEST_EXPONENT = "largest_exponent";
+        public static string SMALLEST_EXPONENT = "smallest_exponent";
+    }
+}
